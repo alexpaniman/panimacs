06dab5d8b90e9086382053d4ef3f5b403a135387
alexpaniman <alexpaniman@gmail.com>
2024-06-23 15:46:07 +0300

    adjustment: ellama switch to llama3 as main model

--- lisp/panimacs-llm.el
+++ lisp/panimacs-llm.el
@@ -1,18 +1,9 @@
 (use-package ellama
   :init
-  ;; setup key bindings
   (setopt ellama-keymap-prefix "C-c e")
 
   (require 'llm-ollama)
-  (setopt ellama-provider
-          (make-llm-ollama :chat-model "llama3:70b")
-	  ;; (make-llm-ollama
-	  ;;  ;; this model should be pulled to use it
-	  ;;  ;; value should be the same as you print in terminal during pull
-	  ;;  :chat-model "llama3-gradient"
-	  ;;  :embedding-model "nomic-embed-text"
-	  ;;  :default-chat-non-standard-params '(("num_ctx" . 90000)))
-          )
+  (setopt ellama-provider (make-llm-ollama :chat-model "llama3"))
   )
 
 (provide 'panimacs-llm)
